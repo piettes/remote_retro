f547965a5ce479562aa435117d9e55c199ca2f4c
Stéphane Piette <stephane.piette@codecentric.de>
2017-07-25 17:11:03 +0200

    fix dist directory

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,7 +14,7 @@ module.exports = {
   entry: "./app/index.tsx",
   output: {
     filename: "bundle.js",
-    path: path.join(__dirname, '..', "dist"),
+    path: path.join(__dirname, "dist"),
     publicPath: '/'
   },
 
